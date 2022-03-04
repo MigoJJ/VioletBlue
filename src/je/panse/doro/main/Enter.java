0eb26864ba6de2a106c484451aba4f6a4b596d93
migoey <jjkoh21@gmail.com>
2022-03-04 07:45:37 -0800

    Access code Input

--- src/je/panse/doro/main/Enter.java
+++ src/je/panse/doro/main/Enter.java
@@ -8,18 +8,18 @@ import java.nio.file.Path;
 public class Enter {
 	//--- user directory definition--------------------------------------------
 	public static String currentUsersHomeDir = System.getProperty("user.dir" );
-	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro/text");
+	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro");
 	
-	public static String wf    = (wd + "/StartMenuList");
-	public static String wcf   = (wd + "/chart.txt");
-	public static String wcc   = (wd + "/form/NEW/1CC");
-	public static String wpmh  = (wd + "/form/NEW/2PMH");	
-	public static String whab  = (wd + "/form/NEW/3HAB");
-	public static String wbmi  = (wd + "/form/NEW/4BMI");
-	public static String wbp   = (wd + "/form/NEW/5BP");
-	public static String wlab  = (wd + "/form/NEW/6LAB");
-	public static String wacc  = (wd + "/form/NEW/7ACC");
-	public static String wplan = (wd + "/form/NEW/8PLAN");
+	public static String wf    = (wd + "/text/StartMenuList");
+	public static String wcf   = (wd + "/text/chart.txt");
+	public static String wcc   = (wd + "/text/form/NEW/1CC");
+	public static String wpmh  = (wd + "/text/form/NEW/2PMH");	
+	public static String whab  = (wd + "/text/form/NEW/3HAB");
+	public static String wbmi  = (wd + "/text/form/NEW/4BMI");
+	public static String wbp   = (wd + "/text/form/NEW/5BP");
+	public static String wlab  = (wd + "/text/form/NEW/6LAB");
+	public static String wacc  = (wd + "/text/form/NEW/7ACC");
+	public static String wplan = (wd + "/text/form/NEW/8PLAN");
     //------------------------------------------------------------------------------------
 	public static void main(String[] args) throws IOException {
 		CurrentDate.defineTime();
