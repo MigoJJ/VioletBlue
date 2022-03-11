ebe826e96f9222530792a95e0fec8f87b116fc6b
migowj <migowj@ubuntu>
2022-03-11 09:29:13 +0900

    merge

--- src/je/panse/doro/comm/ReadWriteToFile.java
+++ src/je/panse/doro/comm/ReadWriteToFile.java
@@ -1,29 +1,40 @@
 package je.panse.doro.comm;
 
-import java.io.IOException;			
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 import je.panse.doro.main.Enter;
 
-public class ReadWriteToFile
-{
-	public static void readWTF(String rwtf) throws IOException
-	{
-		Path fileName = Path.of(rwtf);
-	    String actual  = Files.readString(fileName);
-
-	    System.out.println(actual);
-	    ManageFileAppend.main(actual);
-	}
-	
-	public static void readWTF_DMM(String rwtf_dmm) throws IOException
-	{
-		Path fileName = Path.of(rwtf_dmm);
-	    String actual  = Files.readString(fileName);
-
-	    System.out.println(actual);
-	    ManageFileAppend.main(actual);
-	}
-
-// ------------------------------------------
+public class ReadWriteToFile {
+	  public static void readFile(String readf) throws IOException {
+		    try {
+		      File myObj = new File(readf);
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        System.out.println(data);
+		        
+		      }
+		        myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		  }
+// ----------------------------------------------------
+	  public static void writeFile(String writef, String writed) throws IOException {
+		    try {
+		      FileWriter myWriter = new FileWriter(writef, true);
+		      myWriter.append(writed + "\n");
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file.");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		  }
+// ----------------------------------------------------
 }
