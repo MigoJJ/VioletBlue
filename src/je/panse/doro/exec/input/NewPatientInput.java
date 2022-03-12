fd906643defcf590196d156b9a0d6b2dff22be8c
migowj <migowj@ubuntu>
2022-03-12 12:43:45 +0900

    input 1CC

--- src/je/panse/doro/exec/input/NewPatientInput.java
+++ src/je/panse/doro/exec/input/NewPatientInput.java
@@ -1,21 +1,26 @@
 package je.panse.doro.exec.input;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-import je.panse.doro.comm.ReadWriteToFile;
-import je.panse.doro.main.Enter;
+import je.panse.doro.comm.ManageFile;
 
 public class NewPatientInput {
 // --------------------------------------------	
-	public static void main(String[] args) throws IOException {
-	
-	
-	
-	}
+	public static void main(String n_code) throws IOException {
+		ManageFile.readfiler("/home/migowj/GDS/git/violetBlue0306/src/je/panse/doro/text/ChartInputMenu1");
+				
+		    try (Scanner c_code = new Scanner(System.in)) {
+				System.out.println("Enter selected code number ...   : ");
+				String cn_code = c_code.nextLine();  // Read user input
+				
+				switch (cn_code) {
+						  case "1" : Input1CC.main(n_code);  	break;
+//						  case "2" : SOAPInput.main(n_code);  	break;
+//						  case "3" : SOAPInput.main(n_code);  	break;
+//						  case "4" : SOAPInput.main(n_code);  	break;
+					}
+				}
+			}
 // --------------------------------------------
 }
