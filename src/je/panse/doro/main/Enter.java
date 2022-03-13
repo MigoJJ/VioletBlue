1b44fc07d7e46cadafd6394d632b015bdb199bb8
migoey <migoey@ubuntu>
2022-03-13 05:05:04 -0700

    1

--- src/je/panse/doro/main/Enter.java
+++ src/je/panse/doro/main/Enter.java
@@ -7,30 +7,15 @@ import java.io.IOException;
 public class Enter {
 	// ------------------------------------------------------user directory definition
 	public static String currentUsersHomeDir = System.getProperty("user.dir" );
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public static String wdt = (currentUsersHomeDir + "/src/je/panse/doro/text");
-=======
 	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro");
 
 	public static String wt    = (wd + "/text");
 	public static String wtf    = (wd + "/text/form");
->>>>>>> refs/remotes/origin/frature
-=======
-	public static String wdt = (currentUsersHomeDir + "/src/je/panse/doro/text");
->>>>>>> branch 'master' of https://github.com/MigoJJ/violetBlue0306.git
-	
 	// ------------------------------------------------------file preparing
 	public static void main(String[] args) throws IOException {
        System.out.println(currentUsersHomeDir);
 		CurrentDate.defineTime();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
        System.out.println(currentUsersHomeDir);
->>>>>>> refs/remotes/origin/frature
-=======
->>>>>>> branch 'master' of https://github.com/MigoJJ/violetBlue0306.git
 
 		ManageFile.deletefiler(Enter.wtf + "/ChartPlate");
 		ManageFile.deletefiler(Enter.wtf + "/Comment");
