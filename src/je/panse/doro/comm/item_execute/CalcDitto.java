8cb60bc5b9a18361fb1499e45b28a1c3db05c51e
migoey <migoey@ubuntu>
2022-04-08 05:45:38 -0700

    dose up down

--- src/je/panse/doro/comm/item_execute/CalcDitto.java
+++ src/je/panse/doro/comm/item_execute/CalcDitto.java
@@ -14,25 +14,29 @@ public class CalcDitto {
 	public void main(String args) throws Exception { 
 
 		try (Scanner input = new Scanner(System.in)) {
-	        System.out.print("Input ditto month : ");
-	        dittomo = input.nextInt();
-	        dittomol = ("\t...ditto & OPD F/U x [  " + dittomo +  "  ]-month later");
+			System.out.print("Input ditto month : ");
+			dittomo = input.nextInt();
+			dittomol = ("\t...ditto & OPD F/U x [  " + dittomo +  "  ]-month later");
+			
+			System.out.print("Lab Follow-up will be needed ? :");
+			labfu = input.nextInt();
 	        
-	        System.out.print("Lab Follow-up will be needed ? :");
-	        labfu = input.nextInt();
-	        
-	        if (labfu == 1) {
-			    labful = ("\t...next Lab F/U plan (+)");
-		    }
-	        else {
-	        }
-		    System.out.print("Dose Adjust ? : ");
-		    dosead = input.nextInt();
-	        
-		    if (dosead == 1) {
-		        doseadl = ("\t...no medication change or dose-adjust");
-	        }
-		    else {
+			if (labfu == 0) {   labful = ("\t...Obervation and Follow-up"); }
+			else if (labfu == 1) {   labful = ("\t...next Lab F/U plan (+)"); }
+			else if (labfu == 2) {   labful = ("\t...next 공단검진 plan (+)"); }
+			else if (labfu == 3) {   labful = ("\t...next 지디스검진 plan (+)"); }
+			else {
+			}
+			System.out.print("Dose Adjust ? : ");
+			dosead = input.nextInt();
+
+			if (dosead == 5) { doseadl = ("\t...no medication change or dose-adjust");}
+			else if (dosead == 55) { doseadl = ("\t...New Starting Medication :");}
+			else if (dosead == 6) { doseadl = ("\t...no dose-adjust and continue");}
+			else if (dosead == 2) { doseadl = ("\t...dose-down");}
+			else if (dosead == 8) { doseadl = ("\t...dose-up");}
+			else if (dosead == 4) { doseadl = ("\t...예전 약으로");}
+			else {
 		    }
 			File_cdrw_proc fcp1 = new File_cdrw_proc();
 				fcp1.writeliner(Enter.wts + "/9PLAN", dittomol);
--- src/je/panse/doro/comm/item_execute/CalcHbA1c.java
+++ src/je/panse/doro/comm/item_execute/CalcHbA1c.java
@@ -8,29 +8,27 @@ import je.panse.doro.main.Enter;
 public class CalcHbA1c {
 	public void main(String args) throws Exception { 
 		int Glucose;
+		int Fbspp2;
 		double HbA1c;
+    	String FP = "FBS";
 		String  Gluresult;
-
+ 
     	try (Scanner input = new Scanner(System.in)) {
-	        System.out.print("========= Input Glucose : ");
-	        Glucose = input.nextInt();
-	        
-	        System.out.print("========= Input HbA1c  : ");
-	        HbA1c = input.nextDouble();
+    		System.out.print("========= Input FBS/PP2   Glucose  HBA1c(%)   : ");
+		        Fbspp2 = input.nextInt();
+		        Glucose = input.nextInt();
+		        HbA1c = input.nextDouble();
 //      input.close();
-
-	        Gluresult = ("    FBS       [ " + Glucose +  " ] mg/dl" 
-	        			+ "     HbA1c [ " + HbA1c + " ] %");
-
-			System.out.print(Gluresult);
-			
+		        if (Fbspp2 == 0) {FP = "FBS";}
+		        else {FP = ("PP"+ Fbspp2);}
+		        Gluresult = ("    " + FP + "  [ " + Glucose +  " ] mg/dl"	+ "     HbA1c [ " + HbA1c + " ] %");
 			File_cdrw_proc fcp1 = new File_cdrw_proc();
 				fcp1.writeliner(Enter.wts + "/7LAB", Gluresult);
 			New7LAB.main("");
 			
-			} catch (NumberFormatException e) {
-			e.printStackTrace();
-			}
+		} catch (NumberFormatException e) {
+		e.printStackTrace();
+		}
 	}
 //---------------------------------------------------		
 }
