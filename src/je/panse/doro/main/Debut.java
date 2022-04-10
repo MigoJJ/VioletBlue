5561509cf704a1eb09f79a69ed9f6023ce5bce26
migowj <migowj@ubuntu>
2022-04-11 07:27:35 +0900

    131234

--- src/je/panse/doro/main/Debut.java
+++ src/je/panse/doro/main/Debut.java
@@ -10,22 +10,11 @@ import je.panse.doro.hito.Newcategory;
 
 public class Debut {
 	public void main(String[] args) throws Exception {
-<<<<<<< HEAD
-		DirectoryAdmin de2 = new DirectoryAdmin();
-		Newcategory    de3 = new Newcategory();
-		try (Scanner case_code = new Scanner(System.in)) {
-=======
 		DirectoryAdmin 	de2  = new DirectoryAdmin();
 		Newcategory 		de3  = new Newcategory();
 		File_cw_chart 	fcwc = new File_cw_chart();
 		File_editor_proc  dn4 = new File_editor_proc();
 		try(Scanner case_code = new Scanner(System.in)) {
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/0407_up
-			int select_code = 0;
- 			
-=======
->>>>>>> refs/remotes/origin/0407_up
 			while (true) {
 				System.out.println("Enter selected code number ...   : ");
 				int select_code = Integer.valueOf(case_code.nextLine());
@@ -33,17 +22,13 @@ public class Debut {
 					case 1 : press_Key(Enter.wt + "/singlebeam/subsinglebeam/Prologue");
 					case 2 : press_Key(Enter.wt + "/singlebeam/subsinglebeam/VersionInfo");
 					case 3 : de2.prepare_ChartPlate();
-							  de2.copy_Directory(Enter.wtk + "/hana", Enter.wts);
+							de2.copy_Directory(Enter.wtk + "/hana", Enter.wts);
 							Enter.main(null);  	
 							break;
 					case 4 : de2.copy_Directory(Enter.wtt, Enter.wts);
-<<<<<<< HEAD
-							  de3.main(null);  	
-=======
 							fcwc.writechart();
 							dn4.main(Enter.wt + "/tripikata/ChartPlate");
 							de3.main(null);  	
->>>>>>> refs/remotes/origin/0407_up
 							break; 
 					default :System.out.println(" uncertain ... please check  !!");
 				}
