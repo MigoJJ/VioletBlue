2d25c01c07864eb4e52c3e51fadd9ba9ca5668eb
migowj <migowj@ubuntu>
2022-04-14 09:47:18 +0900

    cbc

--- src/je/panse/doro/hito/Itemcategory.java
+++ src/je/panse/doro/hito/Itemcategory.java
@@ -18,11 +18,13 @@ public class Itemcategory {
 						case 5 : CalcEKG  lab5 = new CalcEKG(); lab5.main("Itemcategorykey");break;
 						case 7 : CalcDEXA lab7 = new CalcDEXA();lab7.main("Itemcategorykey");break;
 						
-					  	case 11 : CalcHbA1c nl2 = new CalcHbA1c();nl2.main("Itemcategorykey");break;
-						case 12 : CalcTFT   nl3 = new CalcTFT(); 	nl3.main("Itemcategorykey");break;
-						case 13 : CalcChol  nl4 = new CalcChol();	nl4.main("Itemcategorykey");break;
-						case 14 : CalcCr  nl5 = new CalcCr();		nl5.main("Itemcategorykey");break;
-						case 15 : CalcVitD  nl6 = new CalcVitD();	nl6.main("Itemcategorykey");break;
+					  	case 11 : CalcHbA1c	nl2 = new CalcHbA1c();nl2.main("Itemcategorykey");break;
+						case 12 : CalcTFT  	nl3 = new CalcTFT(); 	nl3.main("Itemcategorykey");break;
+						case 13 : CalcChol	nl4 = new CalcChol();	nl4.main("Itemcategorykey");break;
+						case 14 : CalcCr  	nl5 = new CalcCr();		nl5.main("Itemcategorykey");break;
+						case 15 : CalcVitD  	nl6 = new CalcVitD();	nl6.main("Itemcategorykey");break;
+						case 16 : CalcCBC  	nl7 = new CalcCBC();		nl7.main(Itemcategorykey);break;
+
 
 						case 9 : case 99 : case 999 :
 					   			Key_Iwbb st999 = new Key_Iwbb();
--- src/je/panse/doro/hito/newsub/New7LAB.java
+++ src/je/panse/doro/hito/newsub/New7LAB.java
@@ -3,6 +3,7 @@ package je.panse.doro.hito.newsub;
 import java.util.Scanner;
 import je.panse.doro.comm.File_cdrw_proc;
 import je.panse.doro.comm.Key_Iwbb;
+import je.panse.doro.comm.item_execute.CalcCBC;
 import je.panse.doro.comm.item_execute.CalcChol;
 import je.panse.doro.comm.item_execute.CalcCr;
 import je.panse.doro.comm.item_execute.CalcHbA1c;
@@ -18,11 +19,12 @@ public class New7LAB {
 				System.out.println("Enter selected code number ...   : ");
 				select_code = Integer.valueOf(new_code.nextLine().trim());
 				switch (select_code) {
-			  	case 1 : CalcHbA1c nl2 = new CalcHbA1c(); 	nl2.main(null);break;
-				case 2 : CalcTFT   nl3 = new CalcTFT(); 		nl3.main(null);break;
-				case 3 : CalcChol  nl4 = new CalcChol();		nl4.main(null);break;
-				case 4 : CalcCr	 nl5 = new CalcCr();		nl5.main(null);break;
-				case 5 : CalcVitD  nl6 = new CalcVitD();		nl6.main(null);break;
+			  	case 1 : CalcHbA1c	nl2 = new CalcHbA1c(); 	nl2.main(null);break;
+				case 2 : CalcTFT   	nl3 = new CalcTFT(); 	nl3.main(null);break;
+				case 3 : CalcChol  	nl4 = new CalcChol();	nl4.main(null);break;
+				case 4 : CalcCr	 	nl5 = new CalcCr();		nl5.main(null);break;
+				case 5 : CalcVitD  	nl6 = new CalcVitD();	nl6.main(null);break;
+				case 6 : CalcCBC  	nl7 = new CalcCBC();		nl7.main(null);break;
 		   		
 		   		case 9 : case 99 : case 999 :
 		   			Key_Iwbb st999 = new Key_Iwbb();
