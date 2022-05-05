62054990a6d3edb8b1c6bed876e4e9cc848be2a5
migowj <jjkoh21@gmail.com>
2022-05-06 07:38:06 +0900

    1CBC

--- src/je/panse/doro/comm/item_execute/CalcCBC.java
+++ src/je/panse/doro/comm/item_execute/CalcCBC.java
@@ -8,11 +8,16 @@ import je.panse.doro.hito.newsub.New7LAB;
 import je.panse.doro.main.Enter;
 
 public class CalcCBC {
+	double Hb;
+	int WBC;
+	int Plt;
+	File_cdrw_proc fcp1 = new File_cdrw_proc();
+
 
 	public void main(String skeys) throws Exception {
-		double Hb;
-		int WBC;
-		int Plt;
+//		double Hb;
+//		int WBC;
+//		int Plt;
 		
     	try (Scanner input = new Scanner(System.in)) {
     		System.out.print("========= Hb    WBC   Platelet : ");
@@ -28,17 +33,21 @@ public class CalcCBC {
 
 			if (WBC == 0 | Plt == 0) {
 				cbchb(rHb);
-			}else {
+			}
+
+			else if (WBC > 0){
+				cbchb(Hb,WBC,Plt);
+			}
+			else {
 				cbchb(rHb,rWBC,rPlt);
 			}
-        
 		if (skeys == "Itemcategorykey") {
-   		File_cdrw_proc fcp1 = new File_cdrw_proc();
+//   		File_cdrw_proc fcp1 = new File_cdrw_proc();
 			fcp1.readfiler(Enter.wt + "/singlebeam/ItemMenu");
 			Itemcategory.main(null);
 		}
 		else {
-			File_cdrw_proc fcp1 = new File_cdrw_proc();
+//			File_cdrw_proc fcp1 = new File_cdrw_proc();
 			fcp1.readfiler(Enter.wt + "/singlebeam/subnewmenu/7LAB_List");
 			New7LAB nc1 = new New7LAB();
 			nc1.main(null);
@@ -57,6 +66,15 @@ public class CalcCBC {
 		fcp1.writeliner(Enter.wts + "/7LAB", rHb);
 		fcp1.writeliner(Enter.wts + "/7LAB", rWBC);
 		fcp1.writeliner(Enter.wts + "/7LAB", rPlt);
+	}
+	static void cbchb(double Hb, int WBC, int Plt) throws Exception {		
+	       String CBCheadline = ("    Hb (g/dl) \t WBC (cells/L) \tPlatelet (billion/L)");
+	       String CBCresult = ("\t" + Hb +"\t\t" + WBC + "\t\t" + Plt);
+				File_cdrw_proc fcp2 = new File_cdrw_proc();
+					fcp2.writeliner(Enter.wts + "/7LAB", CBCheadline);
+					fcp2.writeliner(Enter.wts + "/7LAB", "-----------------------------------------------");
+					fcp2.writeliner(Enter.wts + "/7LAB", CBCresult);
+		
 		}
 // ----------	
 }
