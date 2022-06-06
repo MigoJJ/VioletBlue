b61d27e00d8921994ae5cb1140c0824e1731f1ad
migowj <jjkoh21@gmail.com>
2022-06-06 11:20:54 +0900

    eAG

--- src/je/panse/doro/hito/newsub/New5SUJ.java
+++ src/je/panse/doro/hito/newsub/New5SUJ.java
@@ -1,8 +1,8 @@
 package je.panse.doro.hito.newsub;
 
-import java.io.FileWriter;				
 import java.io.IOException;
 import java.util.Scanner;
+import je.panse.doro.aeternum.aete.DiseaseCode;
 import je.panse.doro.comm.File_cdrw_proc;
 import je.panse.doro.comm.File_cw_chart;
 import je.panse.doro.comm.Menu_list;
@@ -11,40 +11,52 @@ import je.panse.doro.main.Enter;
 
 public class New5SUJ{
 	public static void main(String args, Menu_list suj_insert) throws Exception {
-		String path = (Enter.wts + "/5SUJ");      
-    	String st;
-		try (FileWriter fileWriter = new FileWriter(path,true)) {
+		String st;
+		try {
 			Scanner suj_text = new Scanner(System.in);
-			System.out.println("*****Enter Subjective Complains  ...   : ");
-
+			System.out.println("*****Enter Subjective Complains*****  ...   : ");
 			do{
 				st = (suj_text.nextLine().trim());
 			   if(st.equals("quit")|st.equals("q")|st.equals("ㅂ")){
 			    	System.out.println("Inserted code finished  !  ");
-					Menu_list.main(Enter.wt + "/singlebeam/ChartPopUpMenu");
-			    	File_cw_chart dn1 = new File_cw_chart();
-			    	Newcategory si1= new Newcategory(); 
-					dn1.deuxnine();
-					si1.main(null); 	
+						Menu_list.main(Enter.wt + "/singlebeam/ChartPopUpMenu");
+				    	File_cw_chart dn1 = new File_cw_chart();
+				    	Newcategory si1= new Newcategory(); 
+						dn1.deuxnine();
+						si1.main(null); 	
 			    	break;
 			}
-				System.out.println("st = :  " + st + "\n");
-				fileWriter.append("\n" + st);
-			  New5SUJ.suj_insert(st);
+				if (st.startsWith(":")) {
+					New5SUJ.insertDcode(st);
+				}
+				else {
+					System.out.println("st = :  " + st + "\n");
+					New5SUJ.suj_insert(st);
+				}
 			}while (st != "quit");
-			System.out.println("Success");
-			suj_text.close();
+				System.out.println("Success");
+				suj_text.close();
 		}catch (IOException e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 
 	static void suj_insert(String st) throws IOException {
     	String SUJresult = ("    " + st); 
 		File_cdrw_proc suj1 = new File_cdrw_proc();
-		suj1.writeliner(Enter.wts + "/5SUJ", SUJresult);
+			suj1.writeliner(Enter.wts + "/5SUJ", SUJresult);
 	}
-    // ---------- 
+	
+	static void insertDcode(String ist) throws IOException {
+			String aa = ist.substring(1, ist.length());
+			DiseaseCode e = new DiseaseCode();  
+			System.out.println(e.code_select(aa));
+				ist = e.code_select(aa);
+				System.out.println("st = :  " + ist + "\n");
+			New5SUJ.suj_insert(ist);
+			return;
+		}
+	// ---------- 
 }
 
 	
