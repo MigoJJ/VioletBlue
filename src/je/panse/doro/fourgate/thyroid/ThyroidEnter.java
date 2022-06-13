2373bf2b7c951c2e586ebad832863784217d8d6e
jjkoh <jjkoh@gdclinc.com>
2022-06-13 05:35:06 -0700

    54321

--- src/je/panse/doro/fourgate/thyroid/ThyroidEnter.java
+++ src/je/panse/doro/fourgate/thyroid/ThyroidEnter.java
@@ -12,8 +12,7 @@ public class ThyroidEnter {
 		try(Scanner case_code = new Scanner(System.in)) {
 			while (true) {
 				System.out.println(">>> Enter selected number :  >>>");
-				int select_code = 99;
-				select_code = Integer.valueOf(case_code.nextLine());
+				int select_code = Integer.valueOf(case_code.nextLine());
 				switch (select_code) {
 //					case 1 : press_Key(Enter.wt + "/singlebeam/subsinglebeam/Prologue");
 //					case 2 : press_Key(Enter.wt + "/singlebeam/subsinglebeam/VersionInfo");
