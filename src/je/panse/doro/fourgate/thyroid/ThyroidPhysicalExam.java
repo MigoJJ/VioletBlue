75e3d856c6f29e47feccba26122165889dce4742
migowj <jjkoh21@gmail.com>
2022-06-24 15:54:08 +0900

    thyroid

--- src/je/panse/doro/fourgate/thyroid/ThyroidPhysicalExam.java
+++ src/je/panse/doro/fourgate/thyroid/ThyroidPhysicalExam.java
@@ -1,15 +1,14 @@
 package je.panse.doro.fourgate.thyroid;
 
-import java.io.FileInputStream;	
-import java.io.IOException;
+import java.io.FileInputStream;		
 import java.util.Scanner;
-
 import org.apache.poi.xssf.usermodel.*;
 import je.panse.doro.comm.File_cdrw_proc;
 import je.panse.doro.comm.File_cw_chart;
+import je.panse.doro.comm.Menu_list;
 import je.panse.doro.fourgate.thyroid.thycomm.LmDeux;
 import je.panse.doro.fourgate.thyroid.thycomm.R_rl_excel2;
-import je.panse.doro.hito.Diseasecategory;
+import je.panse.doro.hito.Newcategory;
 import je.panse.doro.main.Enter;
  
 public class ThyroidPhysicalExam {
@@ -48,44 +47,41 @@ public class ThyroidPhysicalExam {
                     }
 	                //--------------for
 //	   	            System.out.println("Insert program ..");
- 			    	Scanner input = new Scanner(System.in);
- 						int excelcolrowchoice=0;
- 			    		System.out.println("\r====== choice the number >>>>>>");
-	        	        excelcolrowchoice = input.nextInt();
-	        	        
-//	        	        System.out.println("cells :" + cells);
-//	        	        System.out.println("rows :" + rows);
-//	        	        System.out.println("rowindex :" + rowindex);
+                    Scanner input = new Scanner(System.in);
+	                    int excelcolrowchoice=0;
+	                    System.out.println("\r====== choice the number >>>>>>");
+	                    excelcolrowchoice = input.nextInt();
 
-						R_rl_excel2 rre1 = new 	R_rl_excel2();
-					    File_cdrw_proc fcp1 = new File_cdrw_proc();
-                    	switch(rowindex) {
-                    		case 0:
-  						       fcp1.writeliner(Enter.wts + "/6OBJ", "\n<Thyroid Physical Exam>\n");
-                    			String eclc =  ( "\tGoiter size  :  [  " + excelcolrowchoice + "  ] cc");
- 						       fcp1.writeliner(Enter.wts + "/6OBJ", eclc);
-		        				break;
-    						case 1 : case 2: case 3 : case 4 : case 5: case 6 : 
-		        				fcp1.writeliner(Enter.wts + "/6OBJ", ".....................................");
-    							rre1.R_rl_excel_point(0,rowindex-1,1);
-    							rre1.R_rl_excel_point(excelcolrowchoice,rowindex-1,1);
-    							break;
-    						case 7 :
-		        				fcp1.writeliner(Enter.wts + "/6OBJ", ".....................................");
-    							rre1.R_rl_excel_point(0,rowindex-1,1);
-    							rre1.R_rl_excel_point(excelcolrowchoice+1,rowindex-1,1);
-    							break;
-     						default:break;
-                    	}
+	                    R_rl_excel2 rre1 = new 	R_rl_excel2();
+	                    File_cdrw_proc fcp1 = new File_cdrw_proc();
+		                    	switch(rowindex) {
+		                    		case 0:
+		  						       fcp1.writeliner(Enter.wts + "/6OBJ", "\n<Thyroid Physical Exam>\n");
+		                    			String eclc =  ( "\tGoiter size  :  [  " + excelcolrowchoice + "  ] cc");
+		 						       fcp1.writeliner(Enter.wts + "/6OBJ", eclc);
+				        				break;
+		    						case 1 : case 2: case 3 : case 4 : case 5: case 6 : 
+				        				fcp1.writeliner(Enter.wts + "/6OBJ", ".....................................");
+		    							rre1.R_rl_excel_point(0,rowindex-1,1);
+		    							rre1.R_rl_excel_point(excelcolrowchoice,rowindex-1,1);
+		    							break;
+		    						case 7 :
+				        				fcp1.writeliner(Enter.wts + "/6OBJ", ".....................................");
+		    							rre1.R_rl_excel_point(0,rowindex-1,1);
+		    							rre1.R_rl_excel_point(excelcolrowchoice+1,rowindex-1,1);
+		    							break;
+		     						default:break;
+		                    	}
    	              //input.close();
                   //====================================if
 	                }
                 }
             LmDeux.main(null);
-            File_cw_chart fcc = new File_cw_chart();
-            fcc.deuxnine();
-            Diseasecategory dc1 = new Diseasecategory();
-            dc1.main(null);
+        	File_cw_chart dn1 = new File_cw_chart();
+        	Newcategory 	cc1 = new Newcategory(); 
+        		dn1.deuxnine();
+        		Menu_list.main(Enter.wt + "/singlebeam/ChartPopUpMenu");
+        		cc1.main(null);
             
 	      }catch(Exception e) {
 	      e.printStackTrace();
