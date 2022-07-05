0269ffc952d52c5881555c1204477910af727186
migowj <jjkoh21@gmail.com>
2022-07-06 07:03:14 +0900

    1234

--- src/je/panse/doro/main/Enter.java
+++ src/je/panse/doro/main/Enter.java
@@ -6,8 +6,8 @@ import je.panse.doro.hito.StartForm;
 public class Enter {
 // ------------------------------------------------------user directory definition
 	public static String currentUsersHomeDir = System.getProperty("user.dir" );
-	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro");
-//	public static String wd = (currentUsersHomeDir + "/je/panse/doro");
+//	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro");
+	public static String wd = (currentUsersHomeDir + "/je/panse/doro");
 
 	public static String wt  = (wd + "/text");
 	public static String wtk = (wd + "/text/knots");
