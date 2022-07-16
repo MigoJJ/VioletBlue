acf8220f26433282b9578a490804905b9d55ada6
jjkoh <jjkoh@gdclinc.com>
2022-07-15 22:19:11 -0700

    first

--- src/je/panse/doro/comm/item_execute/soussymp12/SymptomEnter.java
+++ src/je/panse/doro/comm/item_execute/soussymp12/SymptomEnter.java
@@ -1,33 +1,33 @@
 package je.panse.doro.comm.item_execute.soussymp12;
 
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;	
 
-import je.panse.doro.comm.File_cdrw_proc;
 import je.panse.doro.comm.item_administratus.ClearConsoleScreen;
-import je.panse.doro.hito.Newcategory;
 import je.panse.doro.main.Enter;
 
 public class SymptomEnter {
+	R_rl_excel4 rre1 = new R_rl_excel4();
+	String excelFilePath = (Enter.wd + "/aeternum/dataxlsx/Sympt12List1.xlsx");
+
 	public void main(String[] args) throws Exception {
 		ClearConsoleScreen ccs = new ClearConsoleScreen();
-       ccs.main(null);
-       
-		String excelFilePath = ("/home/migowj/git/Violetblue/src/je/panse/doro/aeternum/dataxlsx/Sympt12List1.xlsx");
-
-		R_rl_excel4 rre1 = new R_rl_excel4();
+		ccs.main(null);
 		rre1.main(0,0,excelFilePath);
+		eppu(null);
+	}
 		
+	private void eppu(String[] eppuNo) throws Exception {
 		Integer submenuNo = 1;
-			try {
+		try {
 				while(submenuNo != 9) {
 					System.out.println(">>> insert code ...");
 					Scanner sc = new Scanner(System.in);
 						submenuNo = sc.nextInt();
 						System.out.println("User input: " + submenuNo);	
-						
 						String ret = R_rl_excel_pick.R_rl_excel_point(submenuNo-1,0,0,excelFilePath);
 						System.out.println("ret    : " + ret);	
-			
 						int colNo=1;
 						if (ret.trim().startsWith("DM")) {	colNo=0;}
 						else if (ret.trim().startsWith("Thy")) {	colNo=1;}
@@ -37,21 +37,59 @@ public class SymptomEnter {
 						else {
 						}
 						rre1.main(1,colNo,excelFilePath);
+						eppu_cell(colNo);
 				}
-		
-//						R_rl_excel3 rre1 = new 	R_rl_excel3();
-//						File_cdrw_proc fcp1 = new File_cdrw_proc();
-//										fcp1.writeliner(Enter.wts + "/9PLAN", "");
-//										rre1.R_rl_excel_point(submenuNo-1,0,0);
-//				}  
-//				File_cdrw_proc fcp1 = new File_cdrw_proc(); 
-//				fcp1.readfiler(Enter.wt + "/singlebeam/ChartPopUpMenu");
-//				Newcategory nc1 = new Newcategory();
-//				nc1.main(null);
+		//		nc1.main(null);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	    }
+	}
+
+	private void eppu_cell(int eppuCellNo) throws Exception {
+		try {
+			
+			System.out.println(">>> insert code ...");
+			Scanner sc = new Scanner(System.in);
+			String sousNo =sc.nextLine();	
+			int sousNoi = Integer.parseInt(sousNo);
+			
+				System.out.println("User input: " + sousNo);	
+				System.out.println("User input: " + sousNoi);	
 		
+//				while(eppuCellNo != 9) {
+					String pret = R_rl_excel_pick.R_rl_excel_point(sousNoi-1,eppuCellNo,1,excelFilePath);
+					System.out.println("ret  pickup  : " + pret);
+					 if (sousNo.startsWith("0")) {
+						 System.out.print( "\t☐" + pret+"\n");
+						 String slschoice = ("\t☐" + pret);
+						 writelinera(Enter.wts + "/5SUJ", slschoice);
+					 }
+					 else {
+						 System.out.print( "\t√" + pret +"\n");
+						 String slschoice = ("\t√" + pret);
+						 writelinera(Enter.wts + "/5SUJ", slschoice);
+					 }
+					
+					
+//				}
+		//		nc1.main(null);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	private void writelinera(String writef, String fts) throws IOException {
+	    try {
+	      FileWriter myWriter = new FileWriter(writef, true);
+//	      myWriter.write("\t"+fts+"\r");
+	      myWriter.write("\t"+fts);
+	      myWriter.close();
+		  System.out.println("   ...Successfully wrote to the file.");
+	    } catch (IOException e) {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	    }
+	}
+//---------------------------
 }
