ce7f5c9c11046ea1c4a14e4ea690d651ed2c7e93
jjkoh <jjkoh@gdclinc.com>
2022-07-21 05:37:49 -0700

    1103

--- src/je/panse/doro/main/SingleBeam.java
+++ src/je/panse/doro/main/SingleBeam.java
@@ -5,9 +5,8 @@ import je.panse.doro.comm.Menu_list;
 public class SingleBeam {
 	public static void main(String[] string) throws Exception {
 		Menu_list.main(Enter.wt + "/singlebeam/SingleBeam");
-		//sf1.readfiler(Enter.wt + "/singlebeam/DoctorLogin");
     	Debut sb2 = new Debut(); 
-	    	sb2.main(null);
+	    sb2.main(null);
 	}
 // ----------
 }
