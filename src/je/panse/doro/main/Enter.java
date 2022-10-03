a18a9c9203ba2b026b5ba0e12168c9b2df1ffcf4
migowj <jjkoh21@gmail.com>
2022-10-04 07:37:42 +0900

    1234

--- src/je/panse/doro/main/Enter.java
+++ src/je/panse/doro/main/Enter.java
@@ -6,8 +6,8 @@ import je.panse.doro.hito.StartForm;
 public class Enter {
 // ------------------------------------------------------user directory definition
 	public static String currentUsersHomeDir = System.getProperty("user.dir" );
-//	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro");
-	public static String wd = (currentUsersHomeDir + "/je/panse/doro");
+	public static String wd = (currentUsersHomeDir + "/src/je/panse/doro");
+//	public static String wd = (currentUsersHomeDir + "/je/panse/doro");
 
 	public static String wt  = (wd + "/text");
 	public static String wtk = (wd + "/text/knots");
@@ -15,8 +15,9 @@ public class Enter {
 	public static String wtt = (wd + "/text/tripikata");
 	public static String wds = (wd + "/aeternum/supportdiagnosis");
 	
-	public static String wtsi = (wt + "/singlebeam");
-	public static String wtss = (wt + "/singlebeam/subnewmenu");
+	public static String wtsi  = (wt + "/singlebeam");
+	public static String wtss  = (wt + "/singlebeam/subnewmenu");
+
 	// ------------------------------------------------------file preparing
 	public static void main(String[] args) throws Exception {	
 //		CurrentDate.defineDate();
