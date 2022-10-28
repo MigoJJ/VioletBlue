57e664979cedc34faf0681e7a60517af292a561e
migowj <jjkoh21@gmail.com>
2022-10-28 12:41:20 +0900

    1

--- src/je/panse/doro/hito/newsub/New6OBJ.java
+++ src/je/panse/doro/hito/newsub/New6OBJ.java
@@ -12,12 +12,11 @@ import je.panse.doro.comm.item_execute.souslab7.CalcEKG;
 public class New6OBJ {
 	public static void main(String skeys) throws Exception {
     	try (Scanner new_code = new Scanner(System.in)) {
+			int select_code = 0;
 			while (true) {
-				int select_code = 0;
 				System.out.println("Enter selected code number ...   : ");
 				select_code = Integer.valueOf(new_code.nextLine().trim());
-				if (new_code.nextLine() != "") {
-					switch (select_code) {
+				switch (select_code) {
 						case 1 : CalcBMI  lab1 = new CalcBMI();	lab1.main(null);break;
 						case 2 : CalcBP   lab2 = new CalcBP(); 	lab2.main(null);break;
 						case 4 : CalcCPA  lab4 = new CalcCPA(); lab4.main(null);break;					
@@ -28,8 +27,6 @@ public class New6OBJ {
 							Key_shortcut.main(select_code);
 					}
 				}
-				Key_shortcut.main(select_code);
-			}	
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
