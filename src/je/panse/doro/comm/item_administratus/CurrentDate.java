302f866f0c4d91b75015efd8fe889552dc3e87b1
migowj <jjkoh21@gmail.com>
2022-12-11 10:43:22 +0900

    SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM");

--- src/je/panse/doro/comm/item_administratus/CurrentDate.java
+++ src/je/panse/doro/comm/item_administratus/CurrentDate.java
@@ -1,6 +1,6 @@
 package je.panse.doro.comm.item_administratus;
 
-import java.util.*;	
+import java.util.*;		
 import java.text.*;
 
 public class CurrentDate {
@@ -8,8 +8,11 @@ public class CurrentDate {
 	
 	public static void defineTime() {
 	      Date dNow = new Date( );
-	      SimpleDateFormat ft =  new SimpleDateFormat ("yyyy.MM.dd E  hh:mm a ");
+//	      SimpleDateFormat ft =  new SimpleDateFormat ("yyyy.MM.dd E  hh:mm a ");
 //	      SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd");
+//	      SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM");
+	      SimpleDateFormat ft = new SimpleDateFormat ("yyyy");
+
 //	      SimpleDateFormat ft = new SimpleDateFormat ("E'요일'  yyyy.MM.dd 'at' hh:mm:ss a zzz");
 	      System.out.println("Current Date: " + ft.format(dNow));
 	      save_time = ft.format(dNow);
---
+++ src/je/panse/doro/comm/item_administratus/CurrentDateTime.java
@@ -0,0 +1,7 @@
+package je.panse.doro.comm.item_administratus;
+
+public class CurrentDateTime {
+	public static void main(String[] args){
+		CurrentDate.defineTime();
+	}
+}
