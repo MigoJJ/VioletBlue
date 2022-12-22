f5ea9b306b04fe8aeb4ef9bb8ec46ad0af6d3039
migowj <migowj@wj-vm>
2022-12-22 09:44:53 +0900

    charttemp

--- src/je/panse/doro/comm/item_administratus/file/File_TextDeleteDuplicate.java
+++ src/je/panse/doro/comm/item_administratus/file/File_TextDeleteDuplicate.java
@@ -1,5 +1,22 @@
 package je.panse.doro.comm.item_administratus.file;
 
+import java.io.*;
+import java.util.HashSet;
+	
 public class File_TextDeleteDuplicate {
-
-}
+	public static void main(String chartplate, String charttemp) throws IOException {
+		try ( PrintWriter outout = new PrintWriter(charttemp);
+			BufferedReader input = new BufferedReader(new FileReader(chartplate))) {
+			String line = input.readLine();
+	       HashSet<String> hs = new HashSet<>();
+	       while (line != null) {
+			   if (hs.add(line)) {
+		         outout.println(line);
+		       }
+		         line = input.readLine();
+	       }
+	      outout.flush();
+		}
+		System.out.println("Finished");
+	    }
+	}	
