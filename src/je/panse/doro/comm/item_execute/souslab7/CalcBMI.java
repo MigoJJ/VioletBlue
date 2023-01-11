ee6a56015d3af9d61e51f04c0371eac9099d6301
migoey <migoey@migoey-vm>
2023-01-11 23:03:17 +0900

    1234

--- src/je/panse/doro/comm/item_execute/souslab7/CalcBMI.java
+++ src/je/panse/doro/comm/item_execute/souslab7/CalcBMI.java
@@ -1,5 +1,6 @@
 package je.panse.doro.comm.item_execute.souslab7;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import je.panse.doro.comm.item_administratus.Menu_list;
@@ -12,7 +13,7 @@ import je.panse.doro.hito.newsub.New6OBJ;
 import je.panse.doro.main.Enter;
 
 public class CalcBMI {
-	File_cdrw_proc fcp1 = new File_cdrw_proc();
+	static File_cdrw_proc fcp1 = new File_cdrw_proc();
 	public void main(String skeys) throws Exception {
 		 try (Scanner input = new Scanner(System.in)) {
 			System.out.print("*****Input weight in kilogram: ");
@@ -35,46 +36,43 @@ public class CalcBMI {
 			bmi bmi1 = new bmi();
 			//  Write to Assessment BMI result
 			CalcBMI.BMItoAssessment(BMI);	
-			
+
 			if (waist == 0) {
 				bmi1.BMIhw(rheight,rweight,BMIresult,skeys);
 			}else {
 				bmi1.BMIhw(rheight,rweight,BMIresult,rwwaist,skeys);
 			}
     }
-
 }
-		static void BMItoAssessment(double bMI) {
-			System.out.println(" BMI result   :   " + bMI);
-		//	fcp1.writeliner(Enter.wts + "/8ASS", "\t# Obesity BMI : [ " + rB+"\t"+ CurrentDateAdd_date.main("m")+" ]");
-		//	fcp1.writeliner(Enter.wts + "/8ASS", "\t# Obesity BMI : [ " + rB+"\t"+ CurrentDateAdd_date.main("m")+" ]");
-				
+		static void BMItoAssessment(double BMI) throws IOException {
+			String result2 = String.format("%.1f", BMI);
+			String bmicategory = "";
+			if (BMI <18.5) {bmicategory ="Underweight";}
+			else if (BMI >= 18.5 & BMI < 25) {bmicategory ="Healthy Weight";}
+			else if (BMI >= 25 & BMI < 30) {bmicategory ="Overweight Weight";}
+			else if (BMI >= 30) {bmicategory ="Obesity";}
+			fcp1.writeliner(Enter.wts + "/8ASS", "\t# " + bmicategory +"[ " + result2 +" ] "+ CurrentDateAdd_date.main("m"));
 		}
 }
 
 class bmi{
+	static File_cdrw_proc fcp1 = new File_cdrw_proc();
 	void BMIhw(String rh, String rw, String rB,String rwa, String skeys) throws Exception {	
 	   String BMIheadline = ("\tHeight(cm)	Weight(kg)	BMI	Waist(cm)");
 	   String Bresult = ("\t" + rh +"\t\t" + rw + "\t\t" + rB + "\t" + rwa);
-	   File_cdrw_proc fcp1 = new File_cdrw_proc();
-		fcp1.writeliner(Enter.wts + "/7LAB", BMIheadline);
-		fcp1.writeliner(Enter.wts + "/7LAB", "\t-----------------------------------Ⓑ");
-		fcp1.writeliner(Enter.wts + "/7LAB", Bresult + "\n");
-
-			fcp1.readfiler(Enter.wtss + "/6OBJ_List");
-			New6OBJ.main(null);
+	   bmi.writeBMI(BMIheadline,Bresult);
 	}
-
 	void BMIhw(String rh, String rw, String rB, String skeys) throws Exception {	
 	   String dliner = "-".repeat(15);
-       String BMIheadline = ("\tHeight(cm)	Weight(kg)	 BMI");
-       String Bresult = ("\t" + rh +" \t\t" + rw + "\t\t" + rB);
-			File_cdrw_proc fcp1 = new File_cdrw_proc();
-				fcp1.writeliner(Enter.wts + "/7LAB", BMIheadline);
-				fcp1.writeliner(Enter.wts + "/7LAB", "\t"+ dliner);
-				fcp1.writeliner(Enter.wts + "/7LAB", Bresult + "\n");
-
-			fcp1.readfiler(Enter.wtss + "/6OBJ_List");
-			New6OBJ.main(null);
-		}
+	   String BMIheadline = ("\tHeight(cm)	Weight(kg)	 BMI");
+	   String Bresult = ("\t" + rh +" \t\t" + rw + "\t\t" + rB);
+	   bmi.writeBMI(BMIheadline,Bresult);
+	}
+	static void writeBMI(String a, String b) throws Exception {
+		fcp1.writeliner(Enter.wts + "/7LAB", a);
+		fcp1.writeliner(Enter.wts + "/7LAB", "\t-----------------------------------Ⓑ");
+		fcp1.writeliner(Enter.wts + "/7LAB", b + "\n");
+		fcp1.readfiler(Enter.wtss + "/6OBJ_List");
+		New6OBJ.main(null);
+	}
 }
