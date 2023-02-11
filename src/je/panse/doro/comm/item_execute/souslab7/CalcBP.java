29bd780b6c9b225a5484226778e51df7857fb54b
migoey <migoey@migoey-vm>
2023-02-12 00:12:19 +0900

    public static String currentUsersHomeDir = System.getProperty("user.dir" );

--- src/je/panse/doro/comm/item_execute/souslab7/CalcBP.java
+++ src/je/panse/doro/comm/item_execute/souslab7/CalcBP.java
@@ -1,14 +1,24 @@
 package je.panse.doro.comm.item_execute.souslab7;
 
+import java.io.File;
 import java.io.IOException;
+
+import je.panse.doro.main.Enter;
 
 public class CalcBP {
 	public static void main(String[] args) throws Exception {
 		    try {
-		      Process p = Runtime.getRuntime().exec("/home/migowj/ittia1_chart/addjar_ittia5/hito/bp java -jar bp_vitalsign.jar");
-		      p.waitFor();
+//		    	File directory = new File(Enter.currentUsersHomeDir + "/ittia1_chart/addjar_ittia5/hito/bp");
+		    	
+		    	File directory = new File("/home/migoey/ittia1_chart/addjar_ittia5/hito/bp");
+
+		    	
+		    	ProcessBuilder builder = new ProcessBuilder("java", "-jar", "Bpmain.jar");
+		    	builder.directory(directory);
+		    	Process process = builder.start();
+		  
 		      System.out.println("The other JAR file has completed execution.");
-		    } catch (IOException | InterruptedException e) {
+		    } catch (IOException e) {
 		      System.out.println("An error occurred while executing the other JAR file.");
 		      e.printStackTrace();
 		    }
