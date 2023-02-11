4088f6141d3498d51fc59edee10e47e390d787fd
migowj <migowj@wj-vm>
2023-02-11 12:46:48 +0900

    1234

--- src/je/panse/doro/comm/item_execute/souslab7/CalcBP.java
+++ src/je/panse/doro/comm/item_execute/souslab7/CalcBP.java
@@ -1,35 +1,17 @@
 package je.panse.doro.comm.item_execute.souslab7;
 
-import java.util.Scanner;	
-
-import je.panse.doro.comm.item_administratus.file.File_cdrw_proc;
-import je.panse.doro.comm.item_administratus.key.Key_Iwbb;
-import je.panse.doro.comm.item_administratus.loop.CurrentDateAdd_date;
-import je.panse.doro.hito.newsub.New6OBJ;
-import je.panse.doro.main.Enter;
+import java.io.IOException;
 
 public class CalcBP {
-	int SBP, DBP, PR;
-	String BPresult;
 	public static void main(String[] args) throws Exception {
-		File_cdrw_proc fcp1 = new File_cdrw_proc(); 
-		Key_Iwbb bb1 = new Key_Iwbb();
-		CalcBP bp1 = new CalcBP();
-
-		try (Scanner input = new Scanner(System.in)) {
-			System.out.print("\r***** Input  [SBP ] [ DBP ]  [pulse rate / minute ] : ");
-			bp1.SBP = input.nextInt();
-			bp1.DBP = input.nextInt();
-			bp1.PR = input.nextInt();
-			//        input.close();
-			bp1.BPresult = String.format("\tBP [ %d / %d ]mmHg   PR [ %d ]/min  Regular LSP", bp1.SBP,bp1.DBP,bp1.PR);
-		   fcp1.writeliner(Enter.wts + "/6OBJ","\tat GDS clinic\t"+ CurrentDateAdd_date.main("h") +"\n");
-		   fcp1.writeliner(Enter.wts + "/6OBJ", bp1.BPresult  + "\n");
-		   fcp1.readfiler(Enter.wtss + "/6OBJ_List");
-			New6OBJ.main(null);
-		} catch (NumberFormatException e) {
-		e.printStackTrace();
-		}
-	}
+		    try {
+		      Process p = Runtime.getRuntime().exec("/home/migowj/ittia1_chart/addjar_ittia5/hito/bp java -jar bp_vitalsign.jar");
+		      p.waitFor();
+		      System.out.println("The other JAR file has completed execution.");
+		    } catch (IOException | InterruptedException e) {
+		      System.out.println("An error occurred while executing the other JAR file.");
+		      e.printStackTrace();
+		    }
+		  }
 //---------------------------------------------------		
 }
