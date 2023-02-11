b7aad6bdfa11f0f81b1a253f3c9de9ab1da18749
migowj <migowj@wj-vm>
2023-02-12 08:44:18 +0900

    1234

--- src/je/panse/doro/comm/item_execute/souslab7/CalcBP.java
+++ src/je/panse/doro/comm/item_execute/souslab7/CalcBP.java
@@ -1,27 +1,27 @@
 package je.panse.doro.comm.item_execute.souslab7;
 
-import java.io.File;
 import java.io.IOException;
 
-import je.panse.doro.main.Enter;
-
 public class CalcBP {
-	public static void main(String[] args) throws Exception {
-		    try {
-//		    	File directory = new File(Enter.currentUsersHomeDir + "/ittia1_chart/addjar_ittia5/hito/bp");
-		    	
-		    	File directory = new File("/home/migoey/ittia1_chart/addjar_ittia5/hito/bp");
-
-		    	
-		    	ProcessBuilder builder = new ProcessBuilder("java", "-jar", "Bpmain.jar");
-		    	builder.directory(directory);
-		    	Process process = builder.start();
-		  
-		      System.out.println("The other JAR file has completed execution.");
-		    } catch (IOException e) {
-		      System.out.println("An error occurred while executing the other JAR file.");
-		      e.printStackTrace();
-		    }
-		  }
-//---------------------------------------------------		
+    public static void main(String[] args) {
+    	        String cmd = "gnome-terminal --working-directory='/home/migowj/ittia1_chart/addjar_ittia5/hito/bp' -e 'bash -c \"command; exec bash\"'";
+    	        try {
+    	            Runtime.getRuntime().exec(cmd);
+    	        } catch (IOException e) {
+    	            e.printStackTrace();
+    	        }
+    	    }
+//
+//    	
+//
+//    	String cd = "cd /home/migowj/ittia1_chart/addjar_ittia5/hito/bp";
+//    	
+//    	String cmd = "gnome-terminal -e 'bash -c \"java -jar /home/migowj/ittia1_chart/addjar_ittia5/hito/bp/vitalsign.jar; exec bash\"'";
+//        try {
+//          Runtime.getRuntime().exec(cd);
+//        	Runtime.getRuntime().exec(cmd);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
