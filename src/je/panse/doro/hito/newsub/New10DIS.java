f0ccba1817e026dd9e1a0d331eb64f41cfb3fe29
migowj <migowj@wj-vm>
2023-02-16 10:41:06 +0900

    1234

--- src/je/panse/doro/hito/newsub/New10DIS.java
+++ src/je/panse/doro/hito/newsub/New10DIS.java
@@ -14,46 +14,55 @@ import je.panse.doro.main.Enter;
 
 public class New10DIS {
 	File_cdrw_proc  ob2 = new File_cdrw_proc();
-	Newcategory 	cc1 = new Newcategory(); 
-	File_cw_chart 	dn1 = new File_cw_chart();
-	
-	public void main(String args) throws Exception { 
-		Menu_list.main(Enter.wtss + "/Menu2ndLine/DisMenu"); 
-		
-    	try (Scanner new_code = new Scanner(System.in)) {
-			int select_code = 0;
-			while (true) {
-				System.out.println(">>>-----Enter selected code number ...   : ");
-				select_code = Integer.valueOf(new_code.nextLine().trim());
-				String tarfile = (Enter.wt+"/knots/dise");
-				switch (select_code) {
-					case 1 : insert_dis("Diabetes Mellitus F/U", tarfile + "/diabetes" );
-					    CalcHbA1c chba1c = new CalcHbA1c();
-					    chba1c.main(null);
-						break;
-					case 2 : insert_dis("Hypertension F/U", tarfile + "/hypertension" );
-						CalcBP cbp = new CalcBP();
-					    cbp.main(null);
-					    break;
-					case 3 : insert_dis("Hypercholestarolemia F/U", tarfile + "/hyperchol" );break;
-					case 41 : insert_dis("Hyperthyroidism F/U", tarfile + "/thyhyper" );break;
-					case 42 : insert_dis("Hypothyroidiam F/U", tarfile + "/thyhypo" );break;
-					case 5 : insert_dis("Osteoporosis F/U", tarfile + "/osteoporosis" );break;
-					default :
-						Key_shortcut.main(select_code);
-				}
-				dn1.deuxnine();
-				Menu_list.main(Enter.wt + "/singlebeam/ChartPopUpMenu");
-				cc1.main(null);
-			}
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		}
-   	}	
-	
+	Newcategory 	  cc1 = new Newcategory(); 
+	File_cw_chart   dn1 = new File_cw_chart();
+
+	public void main(String args) throws Exception {
+	    Menu_list.main(Enter.wtss + "/Menu2ndLine/DisMenu");
+	    try (Scanner new_code = new Scanner(System.in)) {
+	        int select_code = 0;
+	        while (true) {
+	            System.out.println(">>>-----Enter selected code number ...   : ");
+	            select_code = Integer.valueOf(new_code.nextLine().trim());
+	            String tarfile = (Enter.wt + "/knots/dise");
+	            switch (select_code) {
+	                case 1:
+	                    insert_dis("Diabetes Mellitus F/U", tarfile + "/diabetes");
+	                    CalcHbA1c chba1c = new CalcHbA1c();
+	                    chba1c.main(null);
+	                    break;
+	                case 2:
+	                    insert_dis("Hypertension F/U", tarfile + "/hypertension");
+	                    CalcBP cbp = new CalcBP();
+	                    cbp.main(null);
+	                    break;
+	                case 3:
+	                    insert_dis("Hypercholestarolemia F/U", tarfile + "/hyperchol");
+	                    break;
+	                case 41:
+	                    insert_dis("Hyperthyroidism F/U", tarfile + "/thyhyper");
+	                    break;
+	                case 42:
+	                    insert_dis("Hypothyroidiam F/U", tarfile + "/thyhypo");
+	                    break;
+	                case 5:
+	                    insert_dis("Osteoporosis F/U", tarfile + "/osteoporosis");
+	                    break;
+	                default:
+	                    Key_shortcut.main(select_code);
+	            }
+	            dn1.deuxnine();
+	            Menu_list.main(Enter.wt + "/singlebeam/ChartPopUpMenu");
+	            cc1.main(null);
+	        }
+	    } catch (NumberFormatException e) {
+	        e.printStackTrace();
+	    }
+	}
+
 	void insert_dis(String st, String fread) throws IOException {
-    	String ccresult = ("\t" + st); 
-		ob2.writeliner(Enter.wts + "/3CC", ccresult);
-		ob2.rwfiler(fread, Enter.wts + "/9PLAN");
+	    String ccresult = ("\t" + st);
+	    ob2.writeliner(Enter.wts + "/3CC", ccresult);
+	    ob2.rwfiler(fread, Enter.wts + "/9PLAN");
 	}
 }
--- src/je/panse/doro/main/RecoveryNewcategory.java
+++ src/je/panse/doro/main/RecoveryNewcategory.java
@@ -1,7 +1,6 @@
 package je.panse.doro.main;
 
 import java.util.Scanner;
-
 import je.panse.doro.comm.item_administratus.file.File_cdrw_proc;
 import je.panse.doro.comm.item_administratus.file.File_cw_chart;
 import je.panse.doro.comm.item_administratus.key.Key_Iwbb;
@@ -13,45 +12,74 @@ import je.panse.doro.comm.item_subexecute.Edcaproc;
 import je.panse.doro.hito.newsub.*;
 
 public class RecoveryNewcategory {
-// --------------------------------------------	
-	public static void main(String args) throws Exception {
-	    File_cdrw_proc ob2 = new File_cdrw_proc();
-	    ob2.readfiler(Enter.wt + "/singlebeam/ChartPopUpMenu");
 
-	    File_cw_chart ob3 = new File_cw_chart();
-	    ob3.cleanChartPlate();
+    public static void main(String args) throws Exception {
+        File_cdrw_proc ob2 = new File_cdrw_proc();
+        ob2.readfiler(Enter.wt + "/singlebeam/ChartPopUpMenu");
 
-	    File_cw_chart st3 = new File_cw_chart();
-	    st3.callchartlist(Enter.wt + "/stripikata");
+        File_cw_chart ob3 = new File_cw_chart();
+        ob3.cleanChartPlate();
 
-	    switching();
-	}
+        File_cw_chart st3 = new File_cw_chart();
+        st3.callchartlist(Enter.wt + "/stripikata");
+
+        switching();
+    }
+
     static void switching() throws Exception {
-   		Scanner new_code = new Scanner(System.in);
-   		int select_code = 0;
-   		while (true) {
-		   	System.out.println("Enter selected code number ...   : ");
-		   	select_code = Integer.valueOf(new_code.nextLine().trim());
-		   	switch (select_code) {
-	   		case 1 : New3CC nc1 = new New3CC();	 	nc1.main(null);	  	break;
-		   		case 2 : New4PMH.main(null);  	break;
-				case 3 : Edcaproc.main(null, null);  	break;
-		   		case 4 : New6OBJ.main(null);  	break;
-				case 5 : New7LAB.main(null);  	break;
-				case 6 : New8ASS.main(null);  	break;
-				case 7 : New9PLAN.main(null);  	break;
-
-				case 42 : CalcBP ob1 = new CalcBP();	 	ob1.main(null);break;
-				case 51 : CalcHbA1c ob2 = new CalcHbA1c();ob2.main(null);break;
-				case 61 : CalcASS ob3 = new CalcASS();	 	ob3.main(null);break;
-				case 71 : CalcDitto ob4 = new CalcDitto();ob4.main(null);break;
-				
-		   		case 9 : case 99: case 999 :	
-		   			Key_Iwbb st999 = new Key_Iwbb(); 
-		   			st999.Key_Iwbb_Page(select_code); break;
-				default :System.out.println(" uncertain ... please check  !!");
-		   	}
-//		new_code.close();
-   		}
-   	}
+        Scanner new_code = new Scanner(System.in);
+        int select_code = 0;
+        while (true) {
+            System.out.println("Enter selected code number ...   : ");
+            select_code = Integer.valueOf(new_code.nextLine().trim());
+            switch (select_code) {
+                case 1:
+                    New3CC nc1 = new New3CC();
+                    nc1.main(null);
+                    break;
+                case 2:
+                    New4PMH.main(null);
+                    break;
+                case 3:
+                    Edcaproc.main(null, null);
+                    break;
+                case 4:
+                    New6OBJ.main(null);
+                    break;
+                case 5:
+                    New7LAB.main(null);
+                    break;
+                case 6:
+                    New8ASS.main(null);
+                    break;
+                case 7:
+                    New9PLAN.main(null);
+                    break;
+		                case 42:
+		                    CalcBP ob1 = new CalcBP();
+		                    ob1.main(null);
+		                    break;
+		                case 51:
+		                    CalcHbA1c ob2 = new CalcHbA1c();
+		                    ob2.main(null);
+		                    break;
+		                case 61:
+		                    CalcASS ob3 = new CalcASS();
+		                    ob3.main(null);
+		                    break;
+		                case 71:
+		                    CalcDitto ob4 = new CalcDitto();
+		                    ob4.main(null);
+		                    break;
+                case 9:
+                case 99:
+                case 999:
+                    Key_Iwbb st999 = new Key_Iwbb();
+                    st999.Key_Iwbb_Page(select_code);
+                    break;
+                default:
+                    System.out.println(" uncertain ... please check  !!");
+            }
+        }
+    }
 }
