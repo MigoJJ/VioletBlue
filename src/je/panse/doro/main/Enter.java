e151186ad5668c9496f803e568b846433bbbded0
migowj <migowj@wj-vm>
2023-02-19 10:12:11 +0900

    1234

--- src/je/panse/doro/main/Enter.java
+++ src/je/panse/doro/main/Enter.java
@@ -7,9 +7,8 @@ import je.panse.doro.hito.StartForm;
 public class Enter {
 		public static final String currentUsersDir = System.getProperty("user.dir");
 		public static final String currentUsersHomeDir = System.getProperty("user.home");
-
-//		public static final String wd = currentUsersDir + "/src/je/panse/doro";
-		public static final String wd = currentUsersDir + "/je/panse/doro";
+		public static final String wd = currentUsersDir + "/src/je/panse/doro";
+//		public static final String wd = currentUsersDir + "/je/panse/doro";
 		
 		public static final String wt = wd + "/text";
 		public static final String wtk = wd + "/text/knots";
@@ -18,9 +17,9 @@ public class Enter {
 		public static final String wds = wd + "/aeternum/supportdiagnosis";
 		public static final String wtsi = wt + "/singlebeam";
 		public static final String wtss = wt + "/singlebeam/subnewmenu";
-		public static final String udbu = currentUsersHomeDir + "/ittia1_chart/addjar_ittia1";
-		public static final String udbu2 =currentUsersHomeDir + "/ittia1_chart/addjar_ittia2";
-		public static final String udbu5 =currentUsersHomeDir + "/ittia1_chart/addjar_ittia5";
+		public static final String udbu = currentUsersHomeDir + "/ittia_chart/addjar_ittia1";
+		public static final String udbu2 =currentUsersHomeDir + "/ittia_chart/addjar_ittia2";
+		public static final String udbu5 =currentUsersHomeDir + "/ittia_chart/addjar_ittia5";
 
     public static void main(String[] args) throws Exception {
         CurrentDate.defineTime();
